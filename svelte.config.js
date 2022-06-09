3e568f93c8519e6af811636e5696ae17554b80b7
emmepra <matteo.prandi@live.it>
2022-06-10 00:45:03 +0200

    netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess';
 
 /** @type {import('@sveltejs/kit').Config} */
