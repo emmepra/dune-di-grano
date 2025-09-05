fe52b407ce3777b82ff695cf72fb6c3d8a247649
Matteo Prandi <matteo.prandi94@gmail.com>
2025-09-05 13:54:39 +0200

    upd: upgraded app to restore deployability

--- svelte.config.js
+++ svelte.config.js
@@ -1,51 +1,19 @@
-//import adapter from '@sveltejs/adapter-auto';
-import path from "path";
-
-//import adapter from '@sveltejs/adapter-netlify';
 import adapterStatic from '@sveltejs/adapter-static';
-import sveltePreprocess from 'svelte-preprocess';
-
-import svg from "vite-plugin-svgstring";
-// import { PassThrough } from "stream";
-
-let base = "";
-
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sveltePreprocess(),
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapterStatic(),
+		adapter: adapterStatic({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
 		prerender: {
-      		default: true
-    	},
-		files: {
-			assets: 'static',
-			lib: 'src/lib'
-		},
-		vite: {
-			resolve: {
-				alias: {
-					// $lib: path.resolve('./src/lib'),
-					$utils: path.resolve('./src/utils'),
-					$components: path.resolve('./src/components'),
-					$styles: path.resolve('./src/styles'),
-					$bulma: path.resolve('./node_modules/bulma'),
-					$data: path.resolve('./src/data'),
-					//$assets: path.resolve('./static/assets')
-				}
-			},
-			// server: {
-			// 	fs: {
-			// 		allow: ['static/assets/imgs',
-			// 				'static/assets/audio']
-			// 	}
-			// },
-			plugins: [svg()]
-		},
-		paths: {
-			base: '',
-			assets: ''
+			entries: ['*']
 		}
 	}
 };
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import path from 'path';
+import svg from "vite-plugin-svgstring";
+
+export default defineConfig({
+	plugins: [sveltekit(), svg()],
+	resolve: {
+		alias: {
+			$utils: path.resolve('./src/utils'),
+			$components: path.resolve('./src/components'),
+			$styles: path.resolve('./src/styles'),
+			$bulma: path.resolve('./node_modules/bulma'),
+			$data: path.resolve('./src/data')
+		}
+	}
+});
