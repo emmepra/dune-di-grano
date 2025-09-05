caceb0f2f1377c5465afd85b52a391a0e7cb09d3
Matteo Prandi <matteo.prandi94@gmail.com>
2025-09-05 14:03:13 +0200

    fix: added fallback on index.html

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,9 @@ const config = {
 		adapter: adapterStatic({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html',
 			precompress: false,
-			strict: true
+			strict: false
 		}),
 		prerender: {
 			entries: ['*']
